15881ecf718acc3027274a72cf14daa9f25bc740
shayan-github <shayan_a_n@hotmail.com>
2023-04-06 00:53:23 +1000

    Add event listener to number buttons to display the number clicked

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let firstNum;
 let secondNum;
 let operator;
+let displayValue;
+const display = document.querySelector(".display");
+const numberButtons = Array.from(document.querySelectorAll(".number"));
 
 function add(a, b) {
   return a + b;
@@ -21,3 +24,10 @@ function divide(a, b) {
 function operate(operator, firstNum, secondNum) {
   return operator(firstNum, secondNum);
 }
+
+numberButtons.forEach(numberButton => {
+  numberButton.addEventListener("click", () => {
+    display.innerText = numberButton.innerText;
+    displayValue = display.innerText;
+  });
+});
